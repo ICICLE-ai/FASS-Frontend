0fde4946f4bad84aea254496a8280182d93cc66d
chaight <chaight@wisc.edu>
2024-10-17 14:12:32 -0500

    Asthetic changes

--- fass-react/src/App.jsx
+++ fass-react/src/App.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import { Container, Row, Col, Placeholder, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import TopBar from './components/TopBar';
 import AddStoreButton from './components/AddStoreButton';
 import RemoveStoreButton from './components/RemoveStoreButton';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 import MyMapComponent from './components/MyMapComponent';
 
 const App = () => {
   return (
     <div>
-      {/* Top Bar */}
-      <TopBar />
-
       {/* Main Content */}
-      <Container fluid style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Container>
         <Row>
-          <Col md={6}>
+            <TopBar />
+        </Row>
+        <Row>
+          <Col style = {{background:"lightblue"}}>
             {/* Left Side (Other content can go here) */}
-            <h3>Add or Remove Stores</h3>
+            <h3 class="col_header">Add or Remove Stores</h3>
             <div className="d-flex align-items-center flex-column">
             <br/>
             <AddStoreButton/>
@@ -29,10 +28,14 @@ const App = () => {
             </div>
           </Col>
           
-          <Col md={6}>
+          <Col>
             {/* Right Side Image Placeholder */}
-            <h3>Map Placeholder</h3>
+            <h3 class="col_header">Simulation</h3>
             <MyMapComponent/>
+          </Col>
+
+          <Col style = {{background:"lightblue"}}>
+            <h3 class="col_header">Data</h3>
           </Col>
         </Row>
       </Container>
--- fass-react/src/components/MyMapComponent.jsx
+++ fass-react/src/components/MyMapComponent.jsx
@@ -6,7 +6,7 @@ import 'leaflet/dist/leaflet.css';
 
 const MyMapComponent = () => {
   return (
-    <div style={{ height: '500px', width: '500px' }}> {/* Set desired height and width */}
+    <div style={{ height: '75vh', width: '75vh' }}> {/* Set desired height and width */}
         <MapContainer center={[40, -82.92]} zoom={13} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
