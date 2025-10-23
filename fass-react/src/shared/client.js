264fbddd89d0cac2859ddce2d96442f9053a0830
Edric Zhou <ezhou828@gmail.com>
2025-10-23 13:48:53 -0500

    Revert "Update API base URLs for production and staging"
    This reverts commit ee4d45b1759ff121a67431a452ce15faf628d626.

--- fass-react/src/shared/client.js
+++ fass-react/src/shared/client.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 export const client = axios.create({
     // baseURL: 'http://127.0.0.1:8000/api' //staging
-    // baseURL: 'https://fassback.pods.icicleai.tapis.io/api' //prod
+    baseURL: 'https://fassback.pods.icicleai.tapis.io/api' //prod
     // baseURL: 'http://127.0.0.1:8000/api' //local
     // baseURL: 'https://fassback.pods.icicleai.tapis.io/api' //prod
-    baseURL: 'https://fassbackstage.pods.icicleai.tapis.io/api' // staging
+    // baseURL: 'https://fassbackstage.pods.icicleai.tapis.io/api' // staging
     // timeout: 1000,
 });
