bb2433916ccaa87a8875d508f3530e7b77be7ac1
Edric Zhou <ezhou828@gmail.com>
2026-02-20 15:40:37 -0600

    Fixed the aligment issue for households and stores on the map.

--- fass-react/src/components/MapComponent.js
+++ fass-react/src/components/MapComponent.js
@@ -27,8 +27,9 @@ export function initializeMap(mapId, households, stores) {
     // Configure proj4 with the EPSG:3857 and EPSG:4326 projections
     // proj4.defs(EPSG3857, "+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=27445 +datum=WGS84 +units=m +no_defs");
     // proj4.defs(EPSG4326, "+proj=longlat +datum=WGS84 +no_defs");
+    // Use spheres instead of ellipsoid
     proj4.defs('EPSG:3857', '+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs +type=crs');
-proj4.defs('EPSG:4326', '+proj=longlat +datum=WGS84 +no_defs +type=crs');
+    proj4.defs('EPSG:4326', '+proj=longlat +datum=WGS84 +no_defs +type=crs');
 
     const projectToEPSG4326 = (coordinates) => {
         return coordinates.map(coord => {
